51e8ebd2eed0fa9a6ccf2a9f948784b10ee900bd
moorthy21sparkout <163838347+moorthy21sparkout@users.noreply.github.com>
2024-04-17 18:41:03 +0530

    17.04.2024

---
+++ javascript/variable.js
@@ -0,0 +1,111 @@
+
+var a=10;
+var b=20;
+console.log(a);
+//function 
+
+function iphone()
+{
+    console.log("the phone is iphone");
+}
+iphone()
+function redmi()
+{
+    console.log("the phone is redmi");
+}
+redmi()
+
+var a=10;
+var b=24;
+var c=a+b;
+function add(c)
+{
+    console.log("adding of two number is:"+c);
+}
+add(c)
+
+
+
+var factor="thala";
+var fpalyer="thala";
+var fmovie="anbe sivam";
+
+function favirote()
+{
+    console.log("my fav actor is:"+factor);
+    console.log("fav player:"+fpalyer);
+    console.log("fav movie:"+fmovie);
+
+}
+favirote()
+
+function add(a,b)
+{
+    console.log(a+b);
+}
+add ()
+
+function area(len,breadth)
+{
+    console.log("the length is :"+len,"the bradth is :"+breadth)
+}
+area(15,34)
+
+
+function add()
+{
+    var a=10;
+    var b=29;
+    var c=a+b;
+    return c;
+}
+var c=add()
+console.log(c);
+
+
+//if else conditions
+var rain=true;
+if (rain)
+{
+    console.log("yes rain is comming");
+} 
+else
+{
+    console.log("rain is not comming");
+}
+
+
+var year="spring";
+if (year=="spring")
+{
+    console.log("enjoy the blooming the flower")
+} 
+year="summer";
+if (year=="summer")
+{
+    console.log("enjoy the sunshine");
+}
+
+//elseif
+var mark=94;
+if (mark<100 && mark>90)
+{
+    console.log("very good")
+}
+if (mark<90&& mark>80)
+{
+    console.log("good")
+}
+if (mark<80)
+{
+    console.log("very good")
+}
+
+var letter="q";
+if (letter=="a"||letter=="e" || letter=="i" || letter=="o"|| letter=="u")
+{
+    console.log("the letter is vowels")
+}
+else{
+    console.log("the leter is an not a vowels")
+}
