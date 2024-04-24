1ddc44e340a878822740e90ee39278609fc67623
moorthy21sparkout <163838347+moorthy21sparkout@users.noreply.github.com>
2024-04-24 18:37:37 +0530

    24.04.2024

--- javascript/html -css-javascript/mail.php
+++ javascript/html -css-javascript/mail.php
@@ -1,24 +1,26 @@
 <?php
 session_start();
+
 use PHPMailer\PHPMailer\PHPMailer;
+
 require_once 'PHPMailer/src/Exception.php';
 require_once 'PHPMailer/src/PHPMailer.php';
 require_once 'PHPMailer/src/SMTP.php';
-$mail=new PHPMailer(true);
-try{
+$mail = new PHPMailer(true);
+try {
     $mail->isSMTP();
-    $mail->Host='smtp.gmail.com';
-    $mail->SMTPAuth=true;
-    $mail->Username="moorthyponnusamy2019@gmail.com" ;
-    $mail->Password="uqfebwjxipvutgrq";
-    $mail->SMTPSecure="tls";
-    $mail->Port=587;
-    
-    $mail->setfrom($_SESSION['email'] );
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = "moorthyponnusamy2019@gmail.com";
+    $mail->Password = "uqfebwjxipvutgrq";
+    $mail->SMTPSecure = "tls";
+    $mail->Port = 587;
+
+    $mail->setfrom($_SESSION['email']);
     $mail->addAddress("moorthyponnusamy2019@gmail.com");
-     
+
     $mail->isHTML(true);
-    $mail->Subject="message recivied from:".$_SESSION['name'];
+    $mail->Subject = "message recivied from:" . $_SESSION['name'];
     $mail->Body = '
     <b>Name:</b> ' . $_SESSION['name'] . '<br>
     <b>Email:</b> ' . $_SESSION['email'] . '<br>
@@ -31,16 +33,11 @@ try{
 
 
     $mail->send();
-    if( $mail->send())
-    {
+    if ($mail->send()) {
         echo '<script type="text/javascript">alert("Submission successful")</script>';
-    }
-     else {
+    } else {
         echo '<script type="text/javascript">alert("Try again later")</script>';
-     }
-}catch (Exception $e) {
+    }
+} catch (Exception $e) {
     echo '<script type="text/javascript">alert("Error: ' . $e->getMessage() . '")</script>';
 }
-
-?>
-
---
+++ javascript/html -css-javascript/session.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+if (isset($_POST["sub"])) {
+    $_SESSION['name'] = $_POST['name'];
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['phone'] = $_POST['phone'];
+    $_SESSION['gender'] = isset($_POST['gender']) ? $_POST['gender'] : $_POST['gender'];
+    $_SESSION['address'] = $_POST['address'];
+    header("Location:index1.html");
+}
+if (isset($_POST["sub_info"])) {
+    $_SESSION["preferred_contact_datetime"] = $_POST['preferred_contact_datetime'];
+    $_SESSION["contact_type"] = isset($_POST['contact_type']) ? $_POST['contact_type'] : array();
+    $_SESSION["description"] = $_POST['description'];
+    header("Location: mail.php");
+}
